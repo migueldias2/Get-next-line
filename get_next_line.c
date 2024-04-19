5f054f667a199eb9db6b8857cb5c9ec4306b8c66
Miguel Dias <167337540+migueldias2@users.noreply.github.com>
2024-04-20 00:30:47 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcarepa- <mcarepa-@student.42lisboa.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 18:43:00 by mcarepa-          #+#    #+#             */
+/*   Updated: 2024/04/20 00:11:18 by mcarepa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_until_nl(int fd, char *str)
+{
+	char	temp[BUFFER_SIZE + 1];
+	int		chars_read;
+	int		i;
+
+	i = 0;
+	str = NULL;
+	while (i < BUFFER_SIZE + 1)
+		temp[i++] = '\0';
+	chars_read = 1;
+	while (!ft_strchr(temp, '\n') && chars_read)
+	{
+		if (chars_read < 0)
+			return (NULL);
+		chars_read = read(fd, temp, BUFFER_SIZE);
+		if (!str)
+			str = ft_strdup(temp);
+		else
+			str = ft_strjoin(str, temp);
+		if (!str)
+			return (NULL);
+	}
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*string;
+	char		*final_string;
+	char		*remaining_chars;
+	int			i;
+
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	string = get_until_nl(fd, string);
+	if (!string)
+		return (NULL);
+	while (string[i] != '\n' && string[i])
+		i++;
+	if (string[i] == '\n')
+		i++;
+	final_string = ft_substr(string, 0, i);
+	if (!final_string)
+		return (NULL);
+	remaining_chars = ft_substr(string, i, ft_strlen(string) - i);
+	if (!remaining_chars)
+		return (NULL);
+	free(string);
+	string = remaining_chars;
+	return (final_string);
+}
+int	main()
+{
+	int	fd;
+	char	buf[BUFFER_SIZE + 1];
+	int	chars_read;
+	int	line = 1;
+
+	fd = open("file.txt", O_RDONLY);
+	while (chars_read = read(fd, buf, BUFFER_SIZE))
+	{
+		line++;
+		buf[chars_read] = '\0';
+		printf("line %d -> %s", line, buf);
+		free(line);
+	}
+	close (fd)
+	return (0);
+}
---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcarepa- <mcarepa-@student.42lisboa.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 18:43:59 by mcarepa-          #+#    #+#             */
+/*   Updated: 2024/04/20 00:08:11 by mcarepa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char *s1, const char *s2);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+
+#endif
